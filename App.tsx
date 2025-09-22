52fa646eb46d4ff95ac700daf5663ef658dfcf3c
Arthur Brito <arthurbr.lima@gmail.com>
2025-09-22 00:09:42 -0300

    Organização de arquivos + Estilização do app

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './Login';
-import Cadastro from './Cadastro';
-import Home from './Home';
+import Login from './src/pages/teladelogin/Login';
+import Cadastro from './src/pages/cadastro/Cadastro';
+import Home from './src/pages/home/Home';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="Home" component={Home} />
